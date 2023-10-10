4a2009830da715790b9854db71c0217127897004
William Connolly <william.connolly@lithium.com>
2023-10-10 11:52:59 -0500

    Cleanup

--- widget-customizations.js
+++ widget-customizations.js
@@ -6,9 +6,8 @@ window.addEventListener('khorosWidgetOpened', function () {
     modernChatWidget.style.width = "400px";
     modernChatWidget.style.bottom = '0px';
     modernChatWidget.style.right = '20px';
-    let khorosWidgetCloseButton = modernChatWidget.contentWindow.document.getElementById('khWidgetCloseButton')
+    let khorosWidgetCloseButton = modernChatWidget.contentWindow.document.getElementById('khWidgetCloseButton');
     khorosWidgetCloseButton.addEventListener('click', async function (e) {
-        console.log('X clicked');
         e.preventDefault();
         e.stopPropagation();
         getCloseConfirmation();
@@ -40,23 +39,23 @@ function getCloseConfirmation() {
     document.getElementById('web-messenger-container').contentDocument.getElementById('close-main-div')?.remove();
     // Create a div element
     var divMainElement = document.createElement("div");
-    divMainElement.setAttribute('id', 'close-main-div')
+    divMainElement.setAttribute('id', 'close-main-div');
     // Create a paragraph element with text
     var paragraph = document.createElement("p");
     paragraph.textContent = "Close now?";
 
     var divSubElement = document.createElement("div");
-    divSubElement.setAttribute('id', 'close-sub-div')
+    divSubElement.setAttribute('id', 'close-sub-div');
     // Create two buttons
     var buttonOkay = document.createElement("button");
     buttonOkay.textContent = "Close";
-    buttonOkay.setAttribute('id', 'okay-button')
-    buttonOkay.setAttribute('class', 'khorosconfirmationbutton')
+    buttonOkay.setAttribute('id', 'okay-button');
+    buttonOkay.setAttribute('class', 'khorosconfirmationbutton');
 
     var buttonCancel = document.createElement("button");
     buttonCancel.textContent = "Continue";
-    buttonCancel.setAttribute('id', 'cancel-button')
-    buttonCancel.setAttribute('class', 'khorosconfirmationbutton')
+    buttonCancel.setAttribute('id', 'cancel-button');
+    buttonCancel.setAttribute('class', 'khorosconfirmationbutton');
     // Append the paragraph and buttons to the div
     divMainElement.appendChild(paragraph);
     divSubElement.appendChild(buttonOkay);
@@ -65,14 +64,12 @@ function getCloseConfirmation() {
     // Append the div to the document
     document.getElementById('web-messenger-container').contentDocument.getElementById('khChatWindowContainer').insertBefore(divMainElement, document.getElementById('web-messenger-container').contentDocument.getElementById('header').nextSibling);
 
-    let khWidgetCloseOkButton = document.getElementById('web-messenger-container').contentWindow.document.getElementById('okay-button')
-    let khWidgetCloseCancelButton = document.getElementById('web-messenger-container').contentWindow.document.getElementById('cancel-button')
+    let khWidgetCloseOkButton = document.getElementById('web-messenger-container').contentWindow.document.getElementById('okay-button');
+    let khWidgetCloseCancelButton = document.getElementById('web-messenger-container').contentWindow.document.getElementById('cancel-button');
     khWidgetCloseOkButton.addEventListener('click', function (e) {
-        console.log('close clicked');
         getOKConfirmation();
     });
     khWidgetCloseCancelButton.addEventListener('click', function (e) {
-        console.log('continue clicked');
         getCancelConfirmation();
     });
 }
